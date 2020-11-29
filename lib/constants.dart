850ba7c68f2ded4762927d77ae0463834d02b6ed
AAlistrat <aalistrat@gmail.com>
2020-11-29 12:49:11 +0700

    ReadMe change

---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+Color colorGreenLight = Color(0xFF00CE93);
+Color colorGreen = Color(0xFF009A6E);
+Color colorGreenDark = Color(0xFF006A4A);
+Color colorGreen12 = Color(0xFF1FA67F);
+Color colorGreen60 = Color(0xFF99D6C5);
+Color colorGreen80 = Color(0xFFCCEAE2);
+Color colorWhite = Color(0xFFFFFFFF);
+Color colorWhiteBackground = Color(0xFFF4F4F4);
+Color colorGrey12 = Color(0xFFE1E1E1);
+Color colorGrey22 = Color(0xFFC7C7C7);
+Color colorGrey40 = Color(0xFF999999);
+Color colorGrey50 = Color(0xFF797979);
+Color colorGrey85 = Color(0xFF262626);
+Color colorBlack = Color(0xFF000000);
+Color colorYellow = Color(0xFFFFCB00);
+Color colorOrange = Color(0xFFFFA100);
+Color colorRed = Color(0xFFD51C1C);
+
+// TextStyle sfProRegularTextStyle = TextStyle(
+//    fontFamily: '.SF Pro Display',
+//    fontSize: 16,
+//    fontWeight: FontWeight.w300,
+// );
